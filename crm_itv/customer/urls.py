272ac3ec031b97cced9bff6b8b2b499e8ca2d390
sagarikak8130 <sagarikakawade@outlook.com>
2022-02-08 17:41:42 +0530

    align with deployed structure

--- crm_itv/customer/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-from . import views
-app_name = 'customer'
-urlpatterns = [
-	path('ctype/',views.ctype),
-	path('cname/',views.cname,name='cname'),
-	path('ccontact/',views.ccontact,name='ccontact'),
-]
