4b6ff28e4240b4fd3377155e65e2897592a2a770
Aarohi <98arjoshi@gmail.com>
2022-02-09 14:43:23 +0530

    deleted src folder

--- src/main/agency/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from . models import AgencyDetail, AgencyContact
-
-admin.site.register(AgencyDetail)
-admin.site.register(AgencyContact)
