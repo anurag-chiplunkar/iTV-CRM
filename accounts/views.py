906208432e3d1433d33c8bc4d96c52a4f7099637
anurag <59001358+anurag-chiplunkar@users.noreply.github.com>
2022-02-11 12:02:11 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -90,11 +90,18 @@ def emp_login(request):
 			login(request,user)
 			print(request.user)
 			print('You are logged in')
-# 			if email == "shruti@cognitioworld.com" and password == "Cognitio@101$$":
+			if email == "shruti@cognitioworld.com" and password == "Cognitio@101$$":
+				qs1 = Employees.objects.filter(emp_email=email)
+				qs_context = {"qs":qs1}
+
+				for i in qs_context['qs']:
+					email = i.emp_email
+
+				return render(request,'profiles/admin_profile.html',qs_context)
 # 				return render(request,'profiles/admin_profile.html',context)
-# 			elif email == "aarohi@cognitioworld.com" and password == "Cognitio@101$$":
+			elif email == "aarohi@cognitioworld.com" and password == "Cognitio@101$$":
 # 				return render(request,'profiles/admin_profile.html',context)
-# 			elif email == "sanjay@cognitioworld.com" and password == "Cognitio@101$$":
+			elif email == "sanjay@cognitioworld.com" and password == "Cognitio@101$$":
 # 				return render(request,'profiles/admin_profile.html',context)
 
 			qs1 = Employees.objects.filter(emp_email=email)
