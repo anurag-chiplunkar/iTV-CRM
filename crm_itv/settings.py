19e229595fceaacd06aa3514e6f99891822c503d
Aarohi <98arjoshi@gmail.com>
2022-02-11 10:22:47 +0530

    updated profile photo

--- crm_itv/settings.py
+++ crm_itv/settings.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'crm_itv.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'crm_db',
+        'NAME': 'crm',
         'USER': 'root',
         'PASSWORD' :'',
         'HOST' : '',
