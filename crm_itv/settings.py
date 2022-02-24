d5209d58b9363975fdb2293a95afc667cba83795
anurag <59001358+anurag-chiplunkar@users.noreply.github.com>
2022-02-24 13:01:20 +0530

    Update settings.py

--- crm_itv/settings.py
+++ crm_itv/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@(3e5o6)-op@-3lt1m284^=n8^p)d#ngml+2mkoh#9654g%div'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['itv-crm-test.herokuapp.com']
 
